543ca8f4833fe9912f80eba7cec723c3a2c867d8
Jihyeon Choung <wldhgus67@naver.com>
2026-02-16 17:13:59 +0100

    toll project

--- apache_airflow/toll_project/dags/ETL_toll_data.py
+++ apache_airflow/toll_project/dags/ETL_toll_data.py
@@ -1,12 +1,12 @@
-from datetime import timedelta
-from airflow.models import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime,timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
 
 #defining DAG arguments
 default_args = {
     'owner': 'DE1',
-    'start_date': days_ago(0),
+    'start_date': datetime(2026, 2, 15),
     'email': ['your_email_here'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -19,43 +19,43 @@ dag = DAG(
     dag_id = 'ETL_toll_data',
     default_args=default_args,
     description='Apache Airflow Final Assignment',
-    schedule_interval=timedelta(days=1),
+    schedule="@daily",
 )
 
 # define the tasks
 unzip_data = BashOperator(
     task_id='unzip_data',
-    bash_command='tar -xvzf /home/project/airflow/dags/finalassignment/tolldata.tgz',
+    bash_command="bash -c '/opt/airflow/scripts/unzip_data.sh'",
     dag=dag,
 )
 
 extract_data_from_csv = BashOperator(
     task_id = 'extract_data_from_csv',
-    bash_command = 'cut -d"," -f1,2,3,4 vehicle-data.csv > csv_data.csv',
+    bash_command = "bash -c '/opt/airflow/scripts/extract_data_from_csv.sh'",
     dag= dag,
 )
 
 extract_data_from_tsv = BashOperator(
     task_id = 'extract_data_from_tsv',
-    bash_command = "cut -d $'\\t' -f4,5,6 tollplaza-data.tsv > tsv_data.csv",
+    bash_command = "bash -c '/opt/airflow/scripts/extract_data_from_tsv.sh'",
     dag=dag,
 )
 
 extract_data_from_fixed_width = BashOperator(
     task_id = 'extract_data_from_fixed_width',
-    bash_command = 'cut -d " " -f6,7 payment-data.txt > fixed_width_data.csv',
+    bash_command = "bash -c '/opt/airflow/scripts/extract_data_from_fixed_width.sh'",
     dag=dag,
 )
 
 consolidate_data = BashOperator(
     task_id = 'consolidate_data',
-    bash_command = 'paste -d"," csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    bash_command = "bash -c '/opt/airflow/scripts/consolidate_data.sh'",
     dag=dag,
 )
 
 transform_data = BashOperator(
     task_id = 'transform_data',
-    bash_command = 'tr "[a-z]" "[A-Z]" < extracted_data.csv > transformed_data.csv ',
+    bash_command = "bash -c '/opt/airflow/scripts/transform_data.sh'",
     dag=dag,
 )
 
